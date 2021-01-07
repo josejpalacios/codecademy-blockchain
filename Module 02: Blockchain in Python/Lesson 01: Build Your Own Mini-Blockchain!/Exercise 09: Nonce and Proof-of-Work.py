59d358a7e0b74cd143932ebed0375736d83b8eb8
josejpalacios <jose.palacios6119@gmail.com>
2021-01-06 20:48:12 -0600

    Update Exercise 09: Nonce and Proof-of-Work.py

--- Module 02: Blockchain in Python/Lesson 01: Build Your Own Mini-Blockchain!/Exercise 09: Nonce and Proof-of-Work.py
+++ Module 02: Blockchain in Python/Lesson 01: Build Your Own Mini-Blockchain!/Exercise 09: Nonce and Proof-of-Work.py
@@ -1,1 +1,30 @@
+new_transactions = [{'amount': '30', 'sender':'alice', 'receiver':'bob'},
+               	{'amount': '55', 'sender':'bob', 'receiver':'alice'}]
 
+# Task 1: import sha256
+from hashlib import sha256
+
+# Task 2: Create difficulty variable. Sets the amount of leading zeros that must be found in the hash produced by the nonce. Create nonce variable
+difficulty = 2
+nonce = 0
+
+# Task 3: creating the proof
+info = str(nonce) + str(new_transactions)
+# Create hash value
+proof = sha256((info).encode()).hexdigest()
+# printing proof
+print(proof)
+  
+# Task 4: finding a proof that has 2 leading zeros
+while(proof[:2] != '0' * difficulty):
+  # Increment nonce by 1
+  nonce += 1
+  # Convert nonce and new_transactions into strings
+  info = str(nonce) + str(new_transactions)
+  # Create hash value
+  proof = sha256((info).encode()).hexdigest()
+# Store proof into final_proof  
+final_proof = proof
+
+# printing final proof that was found
+print(final_proof)
